8e07b454eb8d4cee142fe15624c52440a6bd9e24
jinzeme <tekintaglobalconcept@gmail.com>
2025-06-10 20:12:31 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,14 @@
-// backend/config/db.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const connectDB = async () => {
-    try {
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        });
-        console.log('MySQL Database connected successfully');
-        return connection;
-    } catch (error) {
-        console.error('Database connection failed:', error);
-        process.exit(1);
-    }
-};
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-export default connectDB;
+export default db;
+
