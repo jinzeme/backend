c4bebc6ff9b701bdb83250d4cba3d7b229313681
jinzeme <tekintaglobalconcept@gmail.com>
2025-06-11 09:29:52 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 import express from 'express';
-const router = express.Router();
 import { getUsers, getUserProfile } from '../controllers/userController.js';
 import authenticateToken from '../middleware/authMiddleware.js';
 
+const router = express.Router();
+
+// ✅ Protect these routes
 router.get('/', authenticateToken, getUsers);
 router.get('/:id', authenticateToken, getUserProfile);
 
